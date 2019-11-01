7f36e1d004aed483f0a8f9a903ae1ba5c17658bb
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-11-01 01:18:27 +0100

    improoving responsivity

--- src/weather.js
+++ src/weather.js
@@ -5,10 +5,10 @@ const Weathers = ({ weathers }) => {
         return (
             <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 card" key={weather.id}>
                 <h3>Weather for: {weather.city}</h3>
-                <p>Actual time: {weather.date}</p>
-                <p>Temperature: {weather.temp} {'\u00b0'}C</p>
-                <p>Pressure: {weather.pressure} hPa</p>
-                <p>Wind: {weather.wind} m/s</p>
+                <p><i class="far fa-clock"></i> Actual time: {weather.date} </p>
+                <p>Temperature: {weather.temp} {'\u00b0'}C <i class="fas fa-thermometer-half"></i></p>
+                <p>Pressure: {weather.pressure} hPa </p>
+                <p>Wind: {weather.wind} m/s <i class="fas fa-wind"></i></p>
             </div>
         )
     })
